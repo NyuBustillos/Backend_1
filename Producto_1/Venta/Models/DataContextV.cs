d85db0fd509c3646bda7d61fe62a640f74ffaca3
Adriana Bustillos <nyubustillos@gmail.com>
2019-04-25 15:16:04 -0400

    Commit MVC VENTA

---
+++ Producto_1/Venta/Models/DataContextV.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Venta.Models
+{
+    public class DataContextV:DbContext
+    {
+        public DataContextV() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Venta.Models.Venta> Ventas { get; set; }
+    }
+}
