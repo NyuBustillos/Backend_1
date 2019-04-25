e0002735b35834b27e63a584594335feab08c2d6
Adriana Bustillos <nyubustillos@gmail.com>
2019-04-25 15:47:08 -0400

    Commit API Cliente

---
+++ Producto_1/ClienteAPI/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace ClienteAPI.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<ClienteAPI.Models.Cliente> Clientes { get; set; }
+    }
+}
