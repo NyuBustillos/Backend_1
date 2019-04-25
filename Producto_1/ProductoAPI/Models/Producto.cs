1ce8cac4ce07bc0500f26a5337f706408440892d
Adriana Bustillos <nyubustillos@gmail.com>
2019-04-25 15:27:44 -0400

    Commit ProductoAPI

---
+++ Producto_1/ProductoAPI/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductoAPI.Models
+{
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+    public class Producto 
+    {
+        [Key]
+        public int ProductoID { get; set; }
+        [Required]
+        public string Nombre_Producto { get; set; }
+        public double Precio_producto{ get; set; }
+        public StatusType Estado { get; set; }
+
+    }
+    
+}
