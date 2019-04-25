995250fb22635a7e9d2d74896d6ea7fe72f72dab
Adriana Bustillos <nyubustillos@gmail.com>
2019-04-25 14:48:38 -0400

    Commit MVC PRODUCTO

---
+++ Producto_1/Cliente/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cliente.Models
+{
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+    public class Cliente
+    {
+        [Key]
+        public int ClienteID { get; set; }
+        [Required]
+        public string NombreCompleto{ get; set; }
+        public string CorreoElectronico{ get; set; }
+        public StatusType Estado { get; set; }
+
+    }
+}
---
+++ Producto_1/Producto_1/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Producto_1.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Producto_1.Models.Producto> Productoes { get; set; }
+    }
+}
