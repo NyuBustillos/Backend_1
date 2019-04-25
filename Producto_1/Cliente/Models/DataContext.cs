0ce186ef8f0921cf1fed47e610425c5a2bd21a0e
Adriana Bustillos <nyubustillos@gmail.com>
2019-04-25 15:03:28 -0400

    Commit MVC CLIENTE

---
+++ Producto_1/Cliente/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Cliente.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Cliente.Models.Cliente> Clientes { get; set; }
+    }
+}
